c33a796bc847185ed58f817f73be3964ee6c138e
Camelia-Andreea Hanes <camelia_hanes@yahoo.ro>
2016-12-24 23:58:00 +0200

    taylor review

--- main.cpp
+++ main.cpp
@@ -1,109 +1,93 @@
 #include <iostream>
-#include <fstream>
+#include <cmath>
 using namespace std;
-ifstream f("farey.in");
-ofstream g("farey.out");
-/*
-void citireDateFisier ( int& n, int& a, int& b ){
-f >> n >> a >> b;
-f.close();
+struct Element
+{
+    int grad;
+    int coeficient;
+};
+
+void CitirePlinom(Element A[], int& lungime)
+{
+    int n;
+    cout<<"Dati lungimea vectorului cu coeficienti: \n";
+    cin>>n;
+    lungime=n;
+    cout<<"\nDati elementele vectorului, sub forma coeficient-grad, incepand de la gradul cel mai mic: \n";
+    for(int i=0; i<=lungime; i++)
+        cin>>A[i].coeficient>>A[i].grad;
+
 }
-*/
-void citireDate ( long& n, long& k){
-f >> n >> k;
-f.close();
+void TparirePolinom(Element A[], int lungime)
+{
+    //tiparirea de face in ordinea descrescatoare a gradelor;
+    cout<<A[lungime].coeficient<<"*X^"<<A[lungime].grad;
+    for(int i=lungime-1; i>=0; i--)
+        cout<<"+"<<A[i].coeficient<<"*X^"<<A[i].grad;
+}
+void DerivarePolinom(Element A[], int& lungime)
+{
+    //functia deriveaza polinomul A dat ca parametru si realizeaza modificari in ce priveste lungimea acestuia
+    if(A[0].grad==0)
+    {
+        for(int i=0; i<lungime; i++)
+        {
+            A[i].coeficient=A[i+1].coeficient*A[i+1].grad;
+            A[i].grad=A[i+1].grad-1;
+        }
+        lungime--;
+    }
+    else{
+        for(int i=0; i<=lungime; i++)
+        {
+            A[i].coeficient*=A[i].grad;
+            A[i].grad--;
+        }
+    }
+
 }
 
-/*
-void fractiaRezultata ( int a, int b, int& aa, int& bb, int n ){
-int af, bf, ai, bi;
-ai = 1;
-af = n-1;
-bf = n;
-bi = n;
-int xi = ai+1;
-int xf = af-1;
-int yi = bf - 1;
-int yf = bf - 1;
-while ( xi != xf ){
-    if ( a == ai && b==bi){
-        aa = xi;
-        bb = yi;
-        return ;
-    }
-    if( a == xf && b == yf ){
-        aa = af;
-        bb = bf;
-        return ;
-    }
-    ai = xi;
-    af = xf;
-    bi = yi;
-    bf = yf;
-    if ( xf - 1 + xi < yf) {
-        xi ++;
-        xf = n-xi;
-        yf = n;
-        yi = n;
-    }else{
-    yi --;
-    yf --;
-    xf = yf-xi;
-    }
+int EvalPoli(Element A[], int lungime, int x)
+{
+int pol=0, q;
+q=pow(x, A[0].grad);
+pol=q*A[0].coeficient;
+for(int i=1; i<=lungime; i++)
+{
+    q*=pow(x, A[i].grad-A[i-1].grad);
+    pol+=q*A[i].coeficient;
 }
-}
-*/
-void fractiaRezultataII ( long k, long& aa, long& bb, long n ){
-long af, bf, ai, bi;
-ai = 1;
-af = n-1;
-bf = n;
-bi = n;
-long i = 1, j = n;
-long xi = ai;
-long xf = af;
-long yi = bf;
-long yf = bf;
-while ( i<j ){
-
-    if ( xf - 1 + xi < yf) {
-        xi ++;
-        xf = n-xi;
-        yf = n;
-        yi = n;
-    }else{
-    yi --;
-    yf --;
-    xf = yf-xi;
-    }
-     if ( i==k ){
-        aa = xi;
-        bb = yi;
-        return ;
-    }
-    if( j==k ){
-        aa = xf;
-        bb = yf;
-        return ;
-    }
-i++;
-j--;
-}
+    return pol;
 }
 
-
-void tiparireFisier ( long a, long b ){
-g << a <<" " <<b;
-g.close();
+int TFX(Element A[], int& lungime, int a, int x)
+{
+    int n=A[lungime].grad; //nr de derivari posibile ale functiei
+    int T=EvalPoli(A, lungime, a);
+    //cout<<T;
+    int  fact=1, p, q=x-a;
+    for(int i=1; i<=n; i++)
+    {
+        DerivarePolinom(A, lungime);
+       p=EvalPoli(A, lungime, a);
+       //cout<<p<<" ";
+       T+=(q*p)/fact;
+       //cout<<"+((x-"<<a<<")^"<<i<<"/"<<fact<<")*"<<p;
+    fact*=(i+1);
+    q*=(x-a);
+    }
+    return T;
 }
+int taylorappprox(int x, int a){
+
+    }
 int main()
 {
-    long n, a, b, aa, bb, k;
-    citireDate(n, k);
-//    citireDateFisier(n, a, b);
-   // fractiaRezultata(a, b, aa, bb, n);
-    fractiaRezultataII(k, aa, bb, n);
-    tiparireFisier(aa, bb);
- //   cout << aa <<" "<<bb;
+   Element A[100];
+   int lungime;
+   CitirePlinom(A, lungime);
+int x, a;
+cin>>a>>x;
+cout<<TFX(A, lungime, a, x);
     return 0;
 }
