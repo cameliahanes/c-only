b69c445f42160fdc6907366ecc8a4ee3aaf7e100
Camelia-Andreea Hanes <camelia_hanes@yahoo.ro>
2016-12-24 23:58:54 +0200

    stirling review

--- main.cpp
+++ main.cpp
@@ -1,93 +1,44 @@
 #include <iostream>
-#include <cmath>
+#define modd 98999
+#include <fstream>
 using namespace std;
-struct Element
-{
-    int grad;
-    int coeficient;
-};
 
-void CitirePlinom(Element A[], int& lungime)
-{
-    int n;
-    cout<<"Dati lungimea vectorului cu coeficienti: \n";
-    cin>>n;
-    lungime=n;
-    cout<<"\nDati elementele vectorului, sub forma coeficient-grad, incepand de la gradul cel mai mic: \n";
-    for(int i=0; i<=lungime; i++)
-        cin>>A[i].coeficient>>A[i].grad;
-
+void precalculareSI (int m[202][202], int n){
+m[1][1] = 1;
+for(int i =2; i<n; i++){
+    for(int j = 1; j<=i; j++)
+        m[i][j] = ( m[i-1][j-1] - (i-1)*m[i-1][j] )%modd;
 }
-void TparirePolinom(Element A[], int lungime)
-{
-    //tiparirea de face in ordinea descrescatoare a gradelor;
-    cout<<A[lungime].coeficient<<"*X^"<<A[lungime].grad;
-    for(int i=lungime-1; i>=0; i--)
-        cout<<"+"<<A[i].coeficient<<"*X^"<<A[i].grad;
-}
-void DerivarePolinom(Element A[], int& lungime)
-{
-    //functia deriveaza polinomul A dat ca parametru si realizeaza modificari in ce priveste lungimea acestuia
-    if(A[0].grad==0)
-    {
-        for(int i=0; i<lungime; i++)
-        {
-            A[i].coeficient=A[i+1].coeficient*A[i+1].grad;
-            A[i].grad=A[i+1].grad-1;
-        }
-        lungime--;
-    }
-    else{
-        for(int i=0; i<=lungime; i++)
-        {
-            A[i].coeficient*=A[i].grad;
-            A[i].grad--;
-        }
-    }
-
 }
 
-int EvalPoli(Element A[], int lungime, int x)
-{
-int pol=0, q;
-q=pow(x, A[0].grad);
-pol=q*A[0].coeficient;
-for(int i=1; i<=lungime; i++)
-{
-    q*=pow(x, A[i].grad-A[i-1].grad);
-    pol+=q*A[i].coeficient;
+void precalculareSII (int m[202][202], int n){
+m[1][1] = 1;
+for(int i=2; i<n; i++){
+    for( int j =1; j<=i; j++)
+        m[i][j] = ( m[i-1][j-1] + j *m[i-1][j] ) % modd;
 }
-    return pol;
 }
-
-int TFX(Element A[], int& lungime, int a, int x)
-{
-    int n=A[lungime].grad; //nr de derivari posibile ale functiei
-    int T=EvalPoli(A, lungime, a);
-    //cout<<T;
-    int  fact=1, p, q=x-a;
-    for(int i=1; i<=n; i++)
-    {
-        DerivarePolinom(A, lungime);
-       p=EvalPoli(A, lungime, a);
-       //cout<<p<<" ";
-       T+=(q*p)/fact;
-       //cout<<"+((x-"<<a<<")^"<<i<<"/"<<fact<<")*"<<p;
-    fact*=(i+1);
-    q*=(x-a);
-    }
-    return T;
-}
-int taylorappprox(int x, int a){
-
-    }
 int main()
 {
-   Element A[100];
-   int lungime;
-   CitirePlinom(A, lungime);
-int x, a;
-cin>>a>>x;
-cout<<TFX(A, lungime, a, x);
+    ifstream f("stirling.in");
+    ofstream g("stirling.out");
+    int n, T;
+    int s[202][202], S[202][202];
+    precalculareSI(s, 202);
+    precalculareSII(S, 202);
+    f >> T;
+    int x, m;
+    while(T--){
+        f >>x >> n>> m;
+        if( x == 1){            g << s[n][m]<<"\n";
+           }
+        else{
+            if( x == 2){
+                g << S[n][m]<<"\n";
+                }
+        }
+    }
+    f.close();
+    g.close();
     return 0;
 }
